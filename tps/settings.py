8f9638f4aa7a6cfff215f0fce1990739ba3c801f
project <zinou.zemir16@gmail.com>
2025-11-25 17:17:52 +0100

    Add gunicorn for Render deployment

--- tps/settings.py
+++ tps/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 import os
 
 # Dossier pour collectstatic
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Dossiers supplémentaires à inclure pour les fichiers statiques
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
@@ -133,6 +133,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
